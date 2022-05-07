ccbac52c2a58d9068aced0bf38bae769513f240c
RinaCelest <celestial.yearly.4@gmail.com>
2022-05-08 00:00:08 +0200

    Update site

--- botconfig.js
+++ botconfig.js
@@ -30,10 +30,10 @@ module.exports = {
   // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "www.lavalinknodepublic.ml",
-    port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "mrextinctcodes", //- The password of the lavalink server.
-    secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
+    host: "lavalink.oops.wtf",
+    port: 2000, // The port that lavalink is listening to. This must be a number!
+    pass: "www.freelavalink.ga", //- The password of the lavalink server.
+    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
   },
 
   //Please go to https://developer.spotify.com/dashboard/
